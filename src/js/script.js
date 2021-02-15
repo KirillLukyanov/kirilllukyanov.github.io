c38ca1e6df74a88c2e914169246329aa84e20cc8
Kirill <kirlukyanov@gmail.com>
2021-02-15 20:55:17 +0300

    footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 800,
+        autoplay: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow_left.svg" alt="Назад"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow_right.svg" alt="Вперед"></button>',
         responsive: [
